afd10ca2280dbb416d9da93b72346aef36177ea5
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-11 11:47:25 +0530

    script

--- script.js
+++ script.js
@@ -5,4 +5,15 @@ var operand1 = 0;
 var operand2 = null;
 var operator = null;
 
-for(var i = 0;)
+for(var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(){
+        var value = this.getAttribute('data-vallue');
+        if(value == '+'){
+
+        } else if(value == '='){
+
+        }else{
+            
+        }
+    })
+}
