cc9c0f4787a0e70bbc360b5cbbf0a3ad56ddd3f5
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-11 11:56:47 +0530

    javascript

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ for(var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(){
         var value = this.getAttribute('data-vallue');
         if(value == '+'){
-
+         operator = '+';
+         operand1 = parseFloat(display.textContent);
         } else if(value == '='){
-
+         operand2 = parseFloat(display.textContent);
         }else{
-            
+            display.innerText += value;
         }
     })
 }
