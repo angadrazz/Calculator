b601c2aeda439db649d64a276a5cf2981ce71f45
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-11 11:39:21 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+var buttons = document.getElementsByClassName("button");
+var display = document.getElementsByClassName("display");
+
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
+
+for(var i = 0;)
